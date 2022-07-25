aca3fe55315da8d447ae7723201e723b4806346b
ppedvLukasK <lukask@ppedv.de>
2022-07-25 09:17:11 +0200

    Initial commit

---
+++ Sprachfeatures/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Sprachfeatures
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			object o = new object();
+			if (o is int)
+			{
+				int x = (int) o;
+			}
+
+			if (o is int x) //auto
+			{
+
+			}
+
+			string text = "123";
+			if (int.TryParse(text, out int ergebnis))
+			{
+
+			}
+		}
+	}
+}
