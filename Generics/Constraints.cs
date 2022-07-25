e6577c1f4a3e39e0ceffbba455fc8fc279cb3cfa
ppedvLukasK <lukask@ppedv.de>
2022-07-25 14:51:49 +0200

    Generics

---
+++ Generics/Constraints.cs
@@ -0,0 +1,65 @@
+﻿namespace Generics;
+
+internal class Constraints
+{
+	int Test = 0;
+
+	public Constraints(int test) => Test = test; //Standardkonstruktor wird entfernt wenn eigener Konstruktor definiert wird
+
+	//spublic Constraints() { }
+
+	static void Main(string[] args)
+	{
+		Point p = new Point(5, 3);
+		Point p2 = p; //Werte werden kopiert
+		p2.X = 10; //X bei Point1 wird nicht verändert (da keine Referenz)
+		//== und != vergleichen die Werte (p.X == p2.X && p.Y == p2.Y)
+
+		Constraints program = new Constraints();
+		Constraints program2 = program; //Referenz zu program wird hergestellt
+		program2.Test = 3; //Test bei Program1 wird verändert, da Referenz
+		//== und != vergleichen HashCodes (program.GetHashCode() == program2.GetHashCode());
+
+		DataStore4<Constraints> ds4; //Nicht möglich ohne Standardkonstruktor
+		DataStore5<DayOfWeek> ds5; //Valide
+		DataStore5<DayOfWeek.Friday> ds52; //Nicht valide
+	}
+
+	public class DataStore1<T> where T : class { } //Referenztyp erzwingen (class)
+
+	public class DataStore2<T> where T : struct { } //Wertetyp erzwingen (struct)
+
+	public class DataStore3<T> where T : Constraints { } //Vererbungshierarchie (Constraints und Unterklassen)
+
+	public class DataStore4<T> where T : new() { } //Nur Typen die einen Default Konstruktor
+
+	public class DataStore5<T> where T : Enum { } //Nur Enums (hier keine Enumwerte)
+
+	public class DataStore6<T> where T : Delegate { } //Nur Delegatetypen (Action, Predicate, Func, eigenes Delegate)
+
+	public class DataStore7<T> where T : unmanaged { } //Bestimmte Typen (z.B.: int, bool, double, ...)
+	//https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/unmanaged-types
+
+	public class DataStore8<T>
+		where T : class, new() //Mehrere Constraints (Ein Referenztyp mit Default Konstruktor)
+	{ }
+
+	public class DataStore9<T1, T2> //Mehrere Constraints auf mehrere Generics
+		where T1 : class, new()
+		where T2 : struct
+	{ }
+
+	public class DataStore<T1, T2, T3, T4, T5, T6, T7> { } //Beliebig viele Generics möglich
+
+	public struct Point
+	{
+		public int X;
+		public int Y;
+
+		public Point(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+	}
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Generics;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		List<string> list = new List<string>(); //Generic: T wird nach unten übernommen (hier T = string);
+		list.Add("123"); //T wird durch string ersetzt: Add(T) -> Add(string)
+
+		Dictionary<string, int> dictionary = new Dictionary<string, int>(); //Klasse mit 2 Generics: TKey (string), TValue (int)
+		dictionary.Add("Max", 1); //string als Key, int als Value: Add(TKey, TValue) -> Add(string, int)
+	}
+}
+
+public class DataStore<T> :
+	IProgress<T>, //T hier übergeben
+	IEquatable<int> //int hier übergeben (fixer Typ)
+{
+	private T[] data = new T[10]; //Array vom Typ T
+
+	public List<T> Data => data.ToList(); //T nach unten in ein weiteres Generic übergeben
+
+	public void Add(int index, T item) => data[index] = item; //Generic in Methodenparameter
+
+	public T GetIndex(int index) //T als return Wert
+	{
+		if (index < 0 || index >= data.Length)
+			return default(T); //default(T): Standardwert von T (int: 0, string: null, bool: false, ...)
+		return data[index];
+	}
+
+	public void PrintType<MyType>() //Generic in Methode
+	{
+		Console.WriteLine(typeof(MyType)); //Typ von T holen
+		Console.WriteLine(nameof(MyType)); //MyType als string: "int", "bool", "Program", ...
+		Console.WriteLine(typeof(MyType).Name); //Selbiges wie nameof
+		Console.WriteLine(default(MyType));
+	}
+
+	public void Report(T value) //T als Parameter durch Vererbung
+	{
+		throw new NotImplementedException();
+	}
+
+	public bool Equals(int other) //Hier int statt T
+	{
+		throw new NotImplementedException();
+	}
+}
+
+public class DataStore2<T> : DataStore<T> { } //Klassen mit T vererben: braucht wieder T beim Klassennamen
